9b775c56a058383e88f79818c16bb50e39116fb0
백채연 <codusl100@naver.com>
2023-01-08 12:14:32 +0900

    project setting

---
+++ relayRun/src/main/java/com/example/relayRun/RelayRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.relayRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayRunApplication.class, args);
+	}
+
+}
---
+++ relayRun/src/test/java/com/example/relayRun/RelayRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relayRun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
