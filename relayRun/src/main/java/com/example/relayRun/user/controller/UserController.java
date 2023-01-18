f339f452ccfa4e4858d5745e94c0dc92ddaaf297
백채연 <codusl100@naver.com>
2023-01-18 12:43:24 +0900

    Refactor : User entity name 컬럼 추가

--- relayRun/src/main/java/com/example/relayRun/user/controller/UserController.java
+++ relayRun/src/main/java/com/example/relayRun/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.relayRun.user.dto.PostUserReq;
 import com.example.relayRun.user.service.UserService;
 import com.example.relayRun.util.BaseException;
 import com.example.relayRun.util.BaseResponse;
+import com.example.relayRun.util.BaseResponseStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,6 +29,17 @@ public class UserController {
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
-
     }
+
+//    @ResponseBody
+//    @PostMapping("/logIn")
+//    public BaseResponse<TokenDto> logIn(@RequestBody PostUserReq user) {
+//        if (user.getEmail().length() == 0 || user.getEmail() == null ) {
+//            return new BaseResponse<>(BaseResponseStatus.POST_USERS_EMPTY_EMAIL);
+//        }
+//        try{
+//            TokenDto token = this.userService.logIn(user);
+//        }
+//    }
+
 }
