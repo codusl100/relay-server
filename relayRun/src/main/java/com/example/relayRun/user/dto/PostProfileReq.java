891a131d172c58a6b6b356b1303b088f6673ec28
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-22 01:54:58 +0900

    Comment:add swagger annotation

--- relayRun/src/main/java/com/example/relayRun/user/dto/PostProfileReq.java
+++ relayRun/src/main/java/com/example/relayRun/user/dto/PostProfileReq.java
@@ -1,5 +1,7 @@
 package com.example.relayRun.user.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostProfileReq {
+    @ApiModelProperty(example = "닉네임")
     private String nickname;
 
+    @ApiModelProperty(example = "상태메세지")
     private String statusMsg;
 
+    @ApiModelProperty(example = "y")
     private String isAlarmOn;
 
+    @ApiModelProperty(example= "이미지 경로")
     private String imgUrl;
 }
