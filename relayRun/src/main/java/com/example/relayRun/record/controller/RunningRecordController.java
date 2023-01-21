469587e5ac82968c8e1417932a1a335235296cb8
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-21 21:27:52 +0900

    feat:running record init edit controller : add annotations and constructor edit service : add annotations and constructor edit repository : add annotations

--- relayRun/src/main/java/com/example/relayRun/record/controller/RunningRecordController.java
+++ relayRun/src/main/java/com/example/relayRun/record/controller/RunningRecordController.java
@@ -1,2 +1,17 @@
-package com.example.relayRun.record.controller;public class RunningRecordController {
+package com.example.relayRun.record.controller;
+
+import com.example.relayRun.record.service.RunningRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/record")
+public class RunningRecordController {
+    RunningRecordService runningRecordService;
+
+    @Autowired
+    public RunningRecordController(RunningRecordService runningRecordService){
+        this.runningRecordService = runningRecordService;
+    }
 }
