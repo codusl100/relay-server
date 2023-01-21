911e8d4bb3a68a470348b569c7c269b363b150f8
Dasom Ahn <lizzy0521@naver.com>
2023-01-21 16:02:52 +0900

    docs : swagger 정보 추가

--- relayRun/src/main/java/com/example/relayRun/club/controller/ClubApplyController.java
+++ relayRun/src/main/java/com/example/relayRun/club/controller/ClubApplyController.java
@@ -6,6 +6,7 @@ import com.example.relayRun.club.service.ClubService;
 import com.example.relayRun.user.entity.UserProfileEntity;
 import com.example.relayRun.util.BaseException;
 import com.example.relayRun.util.BaseResponse;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -25,6 +26,7 @@ public class ClubApplyController {
     // 그룹 생성
     @ResponseBody
     @PostMapping("/{userProfileIdx}/generates")
+    @ApiOperation(value="그룹 생성(방장)", notes="path variable에는 방장 idx, body에는 이름, 소개, 이미지, 최대인원, 레벨, 목표 분류(선택), 목표치 입력")
     public BaseResponse<String> getClubs(Principal principal, @PathVariable("userProfileIdx") Long userProfileIdx, @RequestBody PostClubReq club) {
         try {
             clubService.makesClub(principal, userProfileIdx, club);
