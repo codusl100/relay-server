ed11b8d40ace3dc2a0e59af8455a496148839664
BangChaeul <30cmlive@naver.com>
2023-01-21 21:11:47 +0900

    Fix: TimeTable -> Club

--- relayRun/src/main/java/com/example/relayRun/club/repository/TimeTableRepository.java
+++ relayRun/src/main/java/com/example/relayRun/club/repository/TimeTableRepository.java
@@ -1,6 +1,6 @@
-package com.example.relayRun.timetable.repository;
+package com.example.relayRun.club.repository;
 
-import com.example.relayRun.timetable.entity.TimeTableEntity;
+import com.example.relayRun.club.entity.TimeTableEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TimeTableRepository extends JpaRepository<TimeTableEntity, Long> {
