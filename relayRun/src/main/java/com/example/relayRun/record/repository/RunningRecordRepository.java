2c7b5b78128063387e34d44c4109d87a85f5d30e
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-21 21:27:52 +0900

    feat:running record init edit controller : add annotations and constructor edit service : add annotations and constructor edit repository : add annotations

--- relayRun/src/main/java/com/example/relayRun/record/repository/RunningRecordRepository.java
+++ relayRun/src/main/java/com/example/relayRun/record/repository/RunningRecordRepository.java
@@ -1,2 +1,7 @@
-package com.example.relayRun.record.repository;public interface RunningRecordRepository {
+package com.example.relayRun.record.repository;
+
+import com.example.relayRun.record.entity.RunningRecordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RunningRecordRepository extends JpaRepository<RunningRecordEntity, Long> {
 }
--- relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
+++ relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
@@ -1,2 +1,16 @@
-package com.example.relayRun.record.service;public class RunningRecordService {
+package com.example.relayRun.record.service;
+
+import com.example.relayRun.record.repository.RunningRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RunningRecordService {
+    RunningRecordRepository runningRecordRepository;
+    @Autowired
+    public RunningRecordService(RunningRecordRepository runningRecordRepository) {
+        this.runningRecordRepository = runningRecordRepository;
+    }
+
+
 }
