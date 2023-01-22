cac88a557c26f0c42ab16cb069cd792e458594c0
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-22 13:55:48 +0900

    feat:init_running add DTO

---
+++ relayRun/src/main/java/com/example/relayRun/record/dto/PostRunningInitReq.java
@@ -0,0 +1,15 @@
+package com.example.relayRun.record.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostRunningInitReq {
+    private Long memberStatusIdx;
+
+}
