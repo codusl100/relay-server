091072a62b28e23da8b9178ea2bc65b0525c53a7
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-22 21:05:15 +0900

    feat:add 달리기 시ì api er UI

--- relayRun/src/main/java/com/example/relayRun/record/controller/RunningRecordController.java
+++ relayRun/src/main/java/com/example/relayRun/record/controller/RunningRecordController.java
@@ -5,6 +5,7 @@ import com.example.relayRun.record.dto.PostRunningInitRes;
 import com.example.relayRun.record.service.RunningRecordService;
 import com.example.relayRun.util.BaseException;
 import com.example.relayRun.util.BaseResponse;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class RunningRecordController {
         this.runningRecordService = runningRecordService;
     }
     @PostMapping("/start")
+    @ApiOperation(value="달리기 시작", notes="profileIdx, 속한 clubIdx 요청")
     public BaseResponse<PostRunningInitRes> startRunning(@RequestBody PostRunningInitReq runningInitReq) {
         try{
             PostRunningInitRes result = runningRecordService.startRunning(runningInitReq);
