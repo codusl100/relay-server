8e11fde57dddd4c260a297a3a56db2108a52746a
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-22 14:51:43 +0900

    feat:add findEntityBy group and profile id to membserStatusRepository

--- relayRun/src/main/java/com/example/relayRun/club/repository/MemberStatusRepository.java
+++ relayRun/src/main/java/com/example/relayRun/club/repository/MemberStatusRepository.java
@@ -3,5 +3,8 @@ package com.example.relayRun.club.repository;
 import com.example.relayRun.club.entity.MemberStatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberStatusRepository extends JpaRepository<MemberStatusEntity, Long> {
+    Optional<MemberStatusEntity> findMemberStatusEntityByClubIdxAndUserProfileIdx();
 }
