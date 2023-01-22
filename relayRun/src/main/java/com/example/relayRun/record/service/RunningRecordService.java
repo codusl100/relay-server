ee872e1a1e0bc491fc7b07045f31f72322dbaa50
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-22 14:33:26 +0900

    feat:add memberStatusRepository to RunningRecordService

--- relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
+++ relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
@@ -1,5 +1,6 @@
 package com.example.relayRun.record.service;
 
+import com.example.relayRun.club.repository.MemberStatusRepository;
 import com.example.relayRun.record.repository.RunningRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,9 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class RunningRecordService {
     RunningRecordRepository runningRecordRepository;
+    MemberStatusRepository memberStatusRepository;
     @Autowired
-    public RunningRecordService(RunningRecordRepository runningRecordRepository) {
+    public RunningRecordService(RunningRecordRepository runningRecordRepository,
+                                MemberStatusRepository memberStatusRepository) {
         this.runningRecordRepository = runningRecordRepository;
+        this.memberStatusRepository = memberStatusRepository;
     }
 
 
