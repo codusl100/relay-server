48734dfcd4d6df5d3a45b6ff9f73041cd208a183
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-23 01:35:05 +0900

    fix:edit wrong spell of repository method name

--- relayRun/src/main/java/com/example/relayRun/club/repository/MemberStatusRepository.java
+++ relayRun/src/main/java/com/example/relayRun/club/repository/MemberStatusRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface MemberStatusRepository extends JpaRepository<MemberStatusEntity, Long> {
     List<MemberStatusEntity> findByUserProfileIdx_UserProfileIdx(Long userProfileIdx);
-    Optional<MemberStatusEntity> findByUserProfileIdx_UserProfileIdxAAndApplyStatusIs(Long ProfileIdx, String applyStatus);
+    Optional<MemberStatusEntity> findByUserProfileIdx_UserProfileIdxAndApplyStatusIs(Long ProfileIdx, String applyStatus);
 }
