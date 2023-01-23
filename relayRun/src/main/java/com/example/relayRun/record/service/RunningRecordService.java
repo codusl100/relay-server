be9bf1679da4cd1399359bbf71b0c80ee1f167db
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-24 02:05:52 +0900

    add finishRunning method and apply setter to startRunning

--- relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
+++ relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
@@ -5,14 +5,20 @@ import com.example.relayRun.club.repository.MemberStatusRepository;
 import com.example.relayRun.record.dto.PostRunningFinishReq;
 import com.example.relayRun.record.dto.PostRunningInitReq;
 import com.example.relayRun.record.dto.PostRunningInitRes;
+import com.example.relayRun.record.entity.LocationEntity;
 import com.example.relayRun.record.entity.RunningRecordEntity;
 import com.example.relayRun.record.repository.LocationRepository;
 import com.example.relayRun.record.repository.RunningRecordRepository;
+import com.example.relayRun.record.util.RecordDataHandler;
 import com.example.relayRun.util.BaseException;
 import com.example.relayRun.util.BaseResponseStatus;
+import org.locationtech.jts.io.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -38,13 +44,41 @@ public class RunningRecordService {
             throw new BaseException(BaseResponseStatus.POST_RECORD_INVALID_CLUB_ACCESS);
         }
         MemberStatusEntity memberStatus = optionalMemberStatus.get();
-        RunningRecordEntity recordEntity = RunningRecordEntity.builder()
-                .memberStatusIdx(memberStatus)
-                .distance(0.0f).time(0.0f).pace(0.0f)
-                .build();
+        RunningRecordEntity recordEntity = new RunningRecordEntity();
+        recordEntity.setMemberStatusIdx(memberStatus);
+        recordEntity.setDistance(0.0f);
+        recordEntity.setTime(0.0f);
+        recordEntity.setPace(0.0f);
         recordEntity = runningRecordRepository.save(recordEntity);
         PostRunningInitRes result = new PostRunningInitRes();
         result.setRunningRecordIdx(recordEntity.getRunningRecordIdx());
         return result;
+    }
+
+    public String finishRunning(PostRunningFinishReq runningFinishReq) throws BaseException {
+        Optional<RunningRecordEntity> oldOptionalRecord = runningRecordRepository.findById(runningFinishReq.getRunningRecordIdx());
+        if (oldOptionalRecord.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.POST_RECORD_INVALID_RECORD_ID);
+        }
+        try {
+            RunningRecordEntity oldRecord = oldOptionalRecord.get();
+            List<LocationEntity> locations = RecordDataHandler.toEntityList(runningFinishReq.getLocations());
+            LocalTime timeFormat = LocalTime.parse(
+                    runningFinishReq.getTime(), DateTimeFormatter.ofPattern("HH:mm:ss.nnn")
+            );
+            Float seconds = RecordDataHandler.toSecond(timeFormat);
+            oldRecord.setDistance(runningFinishReq.getDistance());
+            oldRecord.setPace(runningFinishReq.getPace());
+            oldRecord.setTime(seconds);
+            oldRecord.setRunningStatus("finish");
+            for (LocationEntity location : locations) {
+                location.setRecordIdx(oldRecord);
+            }
+            locationRepository.saveAll(locations);
+            runningRecordRepository.save(oldRecord);
+        } catch (ParseException e) {
+            throw new BaseException(BaseResponseStatus.POST_PARSE_ERROR);
+        }
+        return "레코드 저장 성공";
     }
 }
