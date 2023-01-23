d97c323165d31a9508c7498253b37c518df044b8
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-23 15:39:31 +0900

    feat:add locationRepository to RecordService

--- relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
+++ relayRun/src/main/java/com/example/relayRun/record/service/RunningRecordService.java
@@ -2,9 +2,11 @@ package com.example.relayRun.record.service;
 
 import com.example.relayRun.club.entity.MemberStatusEntity;
 import com.example.relayRun.club.repository.MemberStatusRepository;
+import com.example.relayRun.record.dto.PostRunningFinishReq;
 import com.example.relayRun.record.dto.PostRunningInitReq;
 import com.example.relayRun.record.dto.PostRunningInitRes;
 import com.example.relayRun.record.entity.RunningRecordEntity;
+import com.example.relayRun.record.repository.LocationRepository;
 import com.example.relayRun.record.repository.RunningRecordRepository;
 import com.example.relayRun.util.BaseException;
 import com.example.relayRun.util.BaseResponseStatus;
@@ -16,11 +18,14 @@ import java.util.Optional;
 @Service
 public class RunningRecordService {
     RunningRecordRepository runningRecordRepository;
+    LocationRepository locationRepository;
     MemberStatusRepository memberStatusRepository;
     @Autowired
     public RunningRecordService(RunningRecordRepository runningRecordRepository,
+                                LocationRepository locationRepository,
                                 MemberStatusRepository memberStatusRepository) {
         this.runningRecordRepository = runningRecordRepository;
+        this.locationRepository = locationRepository;
         this.memberStatusRepository = memberStatusRepository;
     }
 
