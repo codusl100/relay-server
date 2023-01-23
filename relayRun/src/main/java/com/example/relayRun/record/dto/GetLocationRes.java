e2659ed8b18ef3221d8f2236ca9b247523149a81
Dasom Ahn <lizzy0521@naver.com>
2023-01-24 00:58:31 +0900

    Fix: Location.time format
    array -> yyyy-MM-dd HH:mm:ss

--- relayRun/src/main/java/com/example/relayRun/record/dto/GetLocationRes.java
+++ relayRun/src/main/java/com/example/relayRun/record/dto/GetLocationRes.java
@@ -1,5 +1,6 @@
 package com.example.relayRun.record.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +9,10 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class GetLocationRes {
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time;
+
     private Integer position;
+
     private String status;
 }
