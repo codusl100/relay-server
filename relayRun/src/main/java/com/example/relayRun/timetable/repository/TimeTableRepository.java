613fdfdbaa5dccf9aacc463fa85dba33d60cc81a
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-24 11:45:52 +0900

    feat:add TimeTableRepository

---
+++ relayRun/src/main/java/com/example/relayRun/timetable/repository/TimeTableRepository.java
@@ -0,0 +1,7 @@
+package com.example.relayRun.timetable.repository;
+
+import com.example.relayRun.timetable.entity.TimeTableEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeTableRepository extends JpaRepository<TimeTableEntity, Long> {
+}
