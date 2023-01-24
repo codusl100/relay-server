35228e74b9452c100e0f7d618fff217462401ddc
shortboy7 <thdwldnd7@dgu.ac.kr>
2023-01-24 20:25:53 +0900

    feat:add static util function

--- relayRun/src/main/java/com/example/relayRun/record/util/RecordDataHandler.java
+++ relayRun/src/main/java/com/example/relayRun/record/util/RecordDataHandler.java
@@ -2,10 +2,12 @@ package com.example.relayRun.record.util;
 
 import com.example.relayRun.record.dto.locationDTO;
 import com.example.relayRun.record.entity.LocationEntity;
+import com.example.relayRun.util.GoalType;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -38,5 +40,31 @@ public class RecordDataHandler {
             entities.add(entity);
         }
         return entities;
+    }
+
+    public static int toIntDay(DayOfWeek dayOfWeek) {
+        switch(dayOfWeek) {
+            case MONDAY: return 1;
+            case TUESDAY: return 2;
+            case WEDNESDAY: return 3;
+            case THURSDAY: return 4;
+            case FRIDAY: return 5;
+            case SATURDAY: return 6;
+            case SUNDAY: return 7;
+        }
+        return 0;
+    }
+
+    public static String isSuccess(GoalType type, Float goal, Float time, Float pace, Float distance) {
+        switch(type) {
+            case NOGOAL: return "y";
+            case PACE:
+                return pace >= goal ? "y":"n";
+            case TIME:
+                return time >= goal ? "y":"n";
+            case DISTANCE:
+                return distance >= goal ? "y":"n";
+        }
+        return "y";
     }
 }
