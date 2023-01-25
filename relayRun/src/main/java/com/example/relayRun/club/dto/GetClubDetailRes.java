a6c3e7284a23a3146ce88247337125898e63e927
Yeonji-Lim <view7186@naver.com>
2023-01-26 03:29:46 +0900

    Feat: 그룹 페이지 정보

--- relayRun/src/main/java/com/example/relayRun/club/dto/GetClubDetailRes.java
+++ relayRun/src/main/java/com/example/relayRun/club/dto/GetClubDetailRes.java
@@ -1,2 +1,36 @@
-package com.example.relayRun.club.dto;public class GetClubDetailRes {
+package com.example.relayRun.club.dto;
+
+import com.example.relayRun.util.GoalType;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.List;
+
+public class GetClubDetailRes {
+    @ApiModelProperty(example="그룹 idx")
+    private Long clubIdx;
+
+    @ApiModelProperty(example="그룹 이름")
+    private String name;
+
+    @ApiModelProperty(example="그룹 소개")
+    private String content;
+
+    @ApiModelProperty(example="그룹 이미지 url")
+    private String imgURL;
+
+    @ApiModelProperty(example="그룹 방장 프로필 idx")
+    private Long hostIdx;
+
+    @ApiModelProperty(example="난이도")
+    private Integer level;
+
+    @ApiModelProperty(example="목표 종류")
+    private GoalType goalType;
+
+    @ApiModelProperty(example="목표 km")
+    private Float goal;
+
+//    @ApiModelProperty(example="그룹내 멤버 정보")
+//    private List<GetMemberOfClubRes> memberStatusEntityList;
 }
+
