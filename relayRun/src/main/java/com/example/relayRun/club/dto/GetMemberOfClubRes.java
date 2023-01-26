34e9aa5e37adab4e18755b73b018f8d39edfdf30
yeonji <view7186@naver.com>
2023-01-26 12:29:44 +0900

    Feat: 그룹 페이지에서 필요한 프로필 정보만 추출한 Dto로 변경

--- relayRun/src/main/java/com/example/relayRun/club/dto/GetMemberOfClubRes.java
+++ relayRun/src/main/java/com/example/relayRun/club/dto/GetMemberOfClubRes.java
@@ -1,29 +1,27 @@
 package com.example.relayRun.club.dto;
 
 import com.example.relayRun.record.dto.GetRecordWithoutLocationRes;
-import com.example.relayRun.user.dto.GetProfileRes;
+import com.example.relayRun.user.dto.GetMemberProfileRes;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetMemberOfClubRes {
 
     @ApiModelProperty(example = "지원 상태 idx")
     private Long memberStatusIdx;
 
     @ApiModelProperty(example = "프로필 정보")
-    private GetProfileRes userProfile;
+    private GetMemberProfileRes userProfile;
 
     @ApiModelProperty(example = "해당 프로필 시간표")
-    private GetTimeTableListRes timeTable;
+    private List<GetTimeTableListRes> timeTable;
 
     @ApiModelProperty(example = "당일 달리기 기록")
     private List<GetRecordWithoutLocationRes> runningRecord;
