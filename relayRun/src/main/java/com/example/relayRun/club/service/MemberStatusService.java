0ae50eae86c5d4030968a56b9033b748029cbcd3
Yeonji-Lim <view7186@naver.com>
2023-02-06 17:46:18 +0900

    Fix: applyStatus로 멤버 강퇴하는 것으로 수정
    #77

--- relayRun/src/main/java/com/example/relayRun/club/service/MemberStatusService.java
+++ relayRun/src/main/java/com/example/relayRun/club/service/MemberStatusService.java
@@ -7,12 +7,16 @@ import com.example.relayRun.club.entity.TimeTableEntity;
 import com.example.relayRun.club.repository.ClubRepository;
 import com.example.relayRun.club.repository.MemberStatusRepository;
 import com.example.relayRun.club.repository.TimeTableRepository;
+import com.example.relayRun.user.entity.UserEntity;
 import com.example.relayRun.user.entity.UserProfileEntity;
 import com.example.relayRun.user.repository.UserProfileRepository;
+import com.example.relayRun.user.repository.UserRepository;
 import com.example.relayRun.util.BaseException;
 import com.example.relayRun.util.BaseResponseStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,15 +28,18 @@ public class MemberStatusService {
     private final TimeTableRepository timeTableRepository;
     private final UserProfileRepository userProfileRepository;
     private final ClubRepository clubRepository;
+    private final UserRepository userRepository;
 
     public MemberStatusService(MemberStatusRepository memberStatusRepository,
                                TimeTableRepository timeTableRepository,
                                UserProfileRepository userProfileRepository,
-                               ClubRepository clubRepository) {
+                               ClubRepository clubRepository,
+                               UserRepository userRepository) {
         this.memberStatusRepository = memberStatusRepository;
         this.timeTableRepository = timeTableRepository;
         this.userProfileRepository = userProfileRepository;
         this.clubRepository = clubRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
@@ -203,6 +210,41 @@ public class MemberStatusService {
             }
         } catch (Exception e) {
             throw new BaseException(BaseResponseStatus.POST_TIME_TABLE_FAIL);
+        }
+    }
+
+    public String deleteClubMember(Principal principal, Long clubIdx, PatchDeleteMemberReq request) throws BaseException {
+        Optional<UserEntity> optionalUserEntity = userRepository.findByEmail(principal.getName());
+        if (optionalUserEntity.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.FAILED_TO_LOGIN);
+        }
+        UserEntity userEntity = optionalUserEntity.get();
+
+        Optional<ClubEntity> optionalClubEntity = clubRepository.findByClubIdxAndStatus(clubIdx, "active");
+        if (optionalClubEntity.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.CLUB_UNAVAILABLE);
+        }
+        ClubEntity clubEntity = optionalClubEntity.get();
+
+        if(clubEntity.getHostIdx().getUserIdx().equals(userEntity)) {
+            Optional<MemberStatusEntity> optionalMemberStatusEntity =
+                    memberStatusRepository.findByUserProfileIdx_UserProfileIdxAndApplyStatusAndStatus(
+                            request.getUserProfileIdx(), "ACCEPTED", "active");
+            if (optionalMemberStatusEntity.isEmpty()) {
+                throw new BaseException(BaseResponseStatus.INVALID_MEMBER_STATUS);
+            }
+            MemberStatusEntity memberStatusEntity = optionalMemberStatusEntity.get();
+
+            Optional<UserProfileEntity> optionalUserProfileEntity = userProfileRepository.findByUserProfileIdxAndStatus(request.getUserProfileIdx(), "active");
+            if (optionalUserProfileEntity.isEmpty()) {
+                throw new BaseException(BaseResponseStatus.USER_PROFILE_EMPTY);
+            }
+
+            memberStatusEntity.setApplyStatus("DROPPED");
+            memberStatusRepository.save(memberStatusEntity);
+            return optionalUserProfileEntity.get().getNickName();
+        } else {
+            throw new BaseException(BaseResponseStatus.PATCH_NOT_HOST);
         }
     }
 
