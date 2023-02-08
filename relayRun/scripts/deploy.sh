f254074e2e2ca9cc77b39d0a8f16792ce9242cf3
Dasom Ahn <lizzy0521@naver.com>
2023-02-08 22:03:44 +0900

    Fix: user does not exist error

--- relayRun/scripts/deploy.sh
+++ relayRun/scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 PROJECT_ROOT="/home/ubuntu/app/deploy"
-JAR_NAME="$PROJECT_ROOT/demo-0.0.1-SNAPSHOT.jar"
-echo "> build 파일명 : $JAR_NAME" >> $DEPLOY_LOG
+JAR_NAME="$PROJECT_ROOT/relayRun-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
+echo "> build 파일명 : $JAR_NAME" >> $DEPLOY_LOG
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> $DEPLOY_LOG
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -21,7 +21,6 @@ else
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 # jar 실행
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
