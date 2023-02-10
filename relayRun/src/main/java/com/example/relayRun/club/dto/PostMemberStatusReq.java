fdf2a370d6660e31be286a144c589b112429c973
Dasom Ahn <lizzy0521@naver.com>
2023-02-10 15:28:22 +0900

    Docs: add swagger for club apply

--- relayRun/src/main/java/com/example/relayRun/club/dto/PostMemberStatusReq.java
+++ relayRun/src/main/java/com/example/relayRun/club/dto/PostMemberStatusReq.java
@@ -10,11 +10,9 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ApiModel(value = "그룹 신청 / 시간표 등록 요청 Model")
 public class PostMemberStatusReq {
     @ApiModelProperty(example = "유저의 프로필 인덱스")
     private Long userProfileIdx;
 
-    @ApiModelProperty(example = "시간표 정보")
     private List<TimeTableDTO> timeTables;
 }
